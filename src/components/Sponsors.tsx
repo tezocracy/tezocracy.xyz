54a811194fc12aa595ee34f2e50dc67a8b11ac72
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-10-13 10:00:44 -0700

    fix sponsor links

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -3,18 +3,18 @@ import midlBanner from '../images/midl_baking_banner.png'
 import { Col, Row } from 'react-bootstrap';
 
 function Sponsors() {
-    return (
-        <div className='logos'>
-            <Row>
-                <Col className='text-center'>
-                    <a href="https://midl.dev/" target="_blank"><img src={midlBanner} alt="MIDL.dev" className="shadow p-3 mb-5 bg-body rounded"></img></a>
-                </Col>
-                <Col className='text-center'>
-                    <img src={bakerislandLogo} alt="Bakerisland" className="shadow p-3 mb-5 bg-body rounded"></img>
-                </Col>
-            </Row>
-        </div>
-    )
+  return (
+    <div className='logos'>
+      <Row>
+        <Col className='text-center'>
+          <a href="https://midl.dev/tezos" target="_blank"><img src={midlBanner} alt="MIDL.dev" className="shadow p-3 mb-5 bg-body rounded"></img></a>
+        </Col>
+        <Col className='text-center'>
+          <a href="https://tzstats.com/tz1LVqmufjrmV67vNmZWXRDPMwSCh7mLBnS3"><img src={bakerislandLogo} alt="Bakerisland" className="shadow p-3 mb-5 bg-body rounded"></img></a>
+        </Col>
+      </Row>
+    </div>
+  )
 }
 
 export default Sponsors;
